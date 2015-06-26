343b600313fc49b41536ac8e5ec64cb3b7f15242
atlanlin <ag.yelin@gmail.com>
2015-06-26 14:23:10 +0800

    Latest. Please use this

--- camset.js
+++ camset.js
@@ -9,8 +9,8 @@ var STEP_SIZE_SHUTTER = 1;
 var cam = 1;
 var myInterval;
 var queryString;
-var IMG_WIDTH;
-var IMG_HEIGHT;
+var IMG_WIDTH = 752;
+var IMG_HEIGHT = 752;
 var IMG_ACT_WIDTH;
 var IMG_ACT_HEIGHT;
 var GLOBAL_SCALE = 1;
@@ -116,21 +116,6 @@ function init(){
 		//canvasNode.height = IMG_HEIGHT; // in pixels
 		
 		
-	}
-	
-	function getImgWidth(){
-		var img = document.getElementById("imgSnapshot");
-		return img.clientWidth;
-		
-	
-	}
-	
-	function getImgHeight(){
-		var img = document.getElementById("imgSnapshot");
-		
-		return img.clientHeight;
-	
-	
 	}
 	
 	$("#btnCodeStart").click(function(){
@@ -575,21 +560,21 @@ function setPageScaleSize(resolutionChoice)
 	if(resolutionChoice == 0) // image 640 by 480
 	{
 		mulCenterX = 0.854;
-		mulCenterY = 0.856;
+		mulCenterY = 1;
 		mulOuterRadius = 0.9;
 	
 	}
 	else if(resolutionChoice == 1) //image 1024 by 768
 	{
 		mulCenterX = 1.35667;
-		mulCenterY = 1.374;
+		mulCenterY = 1.6;
 		mulOuterRadius = 1.5;
 	
 	}
 	else if(resolutionChoice == 2) // image 2592 by 1944
 	{
 		mulCenterX = 3.445;
-		mulCenterY = 3.468;
+		mulCenterY = 4.09;
 		mulOuterRadius = 5;
 	
 	}
--- p_to_p.js
+++ p_to_p.js
@@ -523,11 +523,9 @@ function clear(c) {
 function mainDraw() {
   if (canvasValid == false) {
     clear(ctx);
-    
-	if(WIDTH != null && HEIGHT != null){
-		WIDTH = IMG_WIDTH;
-		HEIGHT = IMG_HEIGHT;
-	}
+	
+	WIDTH = IMG_WIDTH;
+	HEIGHT = IMG_HEIGHT;
     // Add stuff you want drawn in the background all the time here
 	
     /*var imageObj = new Image();
