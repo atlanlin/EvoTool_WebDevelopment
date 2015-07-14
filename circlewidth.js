36cdf32c3e4af3638c7dfce025edc334fccdaed8
bernardtanginspiraz <bernard.tang@inspiraz.com>
2015-07-14 09:20:12 +0800

    enable ckp function for circle

--- circlewidth.js
+++ circlewidth.js
@@ -4,8 +4,8 @@ window.onload = function() {
 	//initialize circle on canvas
 	initCircle();
 	
-	//EVOToolName += " " + queryString["toolNo"];
-	//EVOININame +=  " " + queryString["toolNo"];
+	EVOToolName += " " + queryString["toolNo"];
+	EVOININame +=  " " + queryString["toolNo"];
 	
 	//enable function in evo 3 ckp file
 	ajaxGet("info.htm?cmd=%23021%3B"+ EVOToolName +"%3B2%3BGeneral.Enabled%3B1%23");
